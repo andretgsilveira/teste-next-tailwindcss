3fb70d9b2ab421c0a263f5f9766dbb47f647aafe
andretgsilveira <andre_tgsilveira@hotmail.com>
2022-04-25 23:13:21 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing:{
+        '1479': '1479px',
+        '424': '424px'
+      }
+    },
+  },
+  plugins: [],
+}
